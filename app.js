eb4850f6a836e2260147ae1d575dcd401313b4d1
Sardor Khadjimatov <sardorxadjimatov95@gmail.com>
2023-05-27 18:19:48 +0900

    feat: routerni qurish

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
+const router = require("./router.js");
 
 const db = require("./server").db();
 const mongodb = require("mongodb");
@@ -18,4 +19,6 @@ app.set("view engine", "ejs");
 // BSSR -Backend server side rendering
 
 // 4: Routing code
+app.use("/", router);
+
 module.exports = app;
