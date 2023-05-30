da0fcdf500218bdfcca3c514a2221af468af6e51
Sardor Khadjimatov <sardorxadjimatov95@gmail.com>
2023-05-30 18:43:55 +0900

    fix: mongoose connection va member schema model qurish

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const express = require("express");
 const app = express();
 const router = require("./router.js");
 
-const db = require("./server").db();
-const mongodb = require("mongodb");
-
 // 1: Kirish code
 app.use(express.static("public"));
 app.use(express.json());
