7d9c11dbed0e88f51e8017999fed344f29bc3bc1
Sardor Khadjimatov <sardorxadjimatov95@gmail.com>
2023-05-30 18:05:48 +0900

    feat: member controllers

---
+++ controllers/memberController.js
@@ -0,0 +1,18 @@
+let memberController = module.exports;
+
+memberController.home = (req, res) => {
+    console.log("GET cont.home")
+    res.send("Home sahifadasiz");
+};
+memberController.signup = (req, res) => {
+    console.log("POST cont.signup")
+    res.send("Signup sahifadasiz");
+};
+memberController.login = (req, res) => {
+    console.log("POST cont.login")
+    res.send("Login sahifadasiz");
+};
+memberController.logout = (req, res) => {
+    console.log("GET cont.logout")
+    res.send("Logout sahifadasiz");
+};
--- router.js
+++ router.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const memberController = require("./controllers/memberController");
 
-router.get("/", function (req, res) {
-    res.send("Home sahifadasiz");
-});
+// Member related routers
+router.get("/", memberController.home);
+router.post("/signup", memberController.signup);
+router.post("/login", memberController.login);
+router.get("/logout", memberController.logout);
 
+// Other routers
 router.get("/menu", (req, res) => {
     res.send("Menu sahifadasiz");
 });
