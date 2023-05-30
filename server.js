57f588e3c4f8ba5fa49f91d2db51140f4195aafa
Sardor Khadjimatov <sardorxadjimatov95@gmail.com>
2023-05-30 18:43:55 +0900

    fix: mongoose connection hosil qilish

--- server.js
+++ server.js
@@ -2,25 +2,25 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const http = require("http");
-const mongodb = require("mongodb");
+const mongoose = require("mongoose");
 
+let db;
 const connectionString = process.env.MONGO_URL;
-mongodb.connect(
+mongoose.connect(
     connectionString, 
     {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
 }, 
-(err, client) => {
+(err, goose) => {
     if(err) console.log("ERROR on connection MongoDB");
     else {
+        
         console.log("MongoDB connection succeed");
-        console.log(client);
-        module.exports = client;
-
-       const app = require("./app");
-       const server = http.createServer(app);
-        let PORT = process.env.PORT || 3000;
+        //console.log(goose);
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = process.env.PORT || 3003;
         server.listen(PORT, function () {
         console.log(
         `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
