99689625fabb109c801899e7c5064f4e38dce5a7
Sardor Khadjimatov <sardorxadjimatov95@gmail.com>
2023-06-06 19:12:28 +0900

    feat: develop Product controller logic

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -23,7 +23,7 @@ restaurantController.getSignupMyRestaurant = async (req, res) => {
     console.log(`ERROR, cont/getSignupMyRestaurant, ${err.message}`);  
     res.json({state: "fail", message: err.message});
   }
-}
+};
 
 restaurantController.signupProcess = async (req, res) => {
   try {
@@ -49,7 +49,7 @@ restaurantController.getLoginMyRestaurant = async (req, res) => {
     console.log(`ERROR, cont/getLoginMyRestaurant, ${err.message}`);  
     res.json({state: "fail", message: err.message});
   }
-}
+};
 
 restaurantController.loginProcess = async (req, res) => {
     try {
@@ -61,14 +61,33 @@ restaurantController.loginProcess = async (req, res) => {
       req.session.member = result;
       req.session.save(function () {
         res.redirect("/resto/products/menu");
-      });
+      });   
     } catch(err) {
       console.log(`ERROR, cont/login, ${err.message}`);
       res.json({ state: "fail", message: err.message});
     }
 };
 
-restaurantController.logoutProcess = (req, res) => {
+restaurantController.logoutProcess = async (req, res) => {
   console.log("GET: cont/logout");
   res.send("logout sahifadasiz");
+};
+
+restaurantController.validateAuthRestaurant = async (req, res, next) => {
+  if (req.session?.member?.mb_type === "RESTAURANT") {
+    req.member = req.session.member;
+    next();
+  } else 
+  res.json({
+    state: "fail", 
+    message: "only authenticated members with restaurant type",
+  });
+};
+
+restaurantController.checkSessions = async (req, res) => {
+  if(req.session?.member) {
+    res.json({state: "succeed", data: req.session.member});
+  } else {
+    res.json({ state: "fail", message: "You are not authenticated" });
+  }
 };
--- router_bssr.js
+++ router_bssr.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const router_bssr = express.Router();
 const restaurantController = require("./controllers/restaurantController");
+const productController = require("./controllers/productController");
 
 /**********************************
  *         BSSR EJS               *
  **********************************/
 
-router_bssr.get("/signup", restaurantController.getSignupMyRestaurant);
-router_bssr.post("/signup", restaurantController.signupProcess);
-
-router_bssr.get("/login", restaurantController.getLoginMyRestaurant);
-router_bssr.post("/login", restaurantController.loginProcess);
-
+router_bssr
+.get("/signup", restaurantController.getSignupMyRestaurant)
+.post("/signup", restaurantController.signupProcess);
+router_bssr
+.get("/login", restaurantController.getLoginMyRestaurant)
+.post("/login", restaurantController.loginProcess);
 router_bssr.get("/logout", restaurantController.logoutProcess);
+router_bssr.get("/check-me", restaurantController.checkSessions);
 
-
-router_bssr.get("/products/menu", restaurantController.getMyRestaurantdata);
-
+router_bssr.get("/products/menu", restaurantController.getMyRestaurantdata);  
+router_bssr.post(
+    "/products/create", 
+    restaurantController.validateAuthRestaurant, 
+    productController.addNewProduct
+    );
+router_bssr.post("/products/edit/:id", productController.updateChosenProduct);
 
 module.exports = router_bssr;
