0a5be2e334799cb682eae5d173710d4c16307931
Sardor Khadjimatov <sardorxadjimatov95@gmail.com>
2023-06-10 19:27:52 +0900

    feat: modified: product.js

--- models/Product.js
+++ models/Product.js
@@ -4,46 +4,56 @@ const Definer = require("../lib/mistake");
 const ProductModel = require("../schema/product.model");
 
 class Product {
-    constuctor() {
-        this.productModel = ProductModel;
+  constructor() {
+    this.productModel = ProductModel;
+  }
+
+  async getAllProductsDataResto(member) {
+    try {
+      member._id = shapeIntoMongooseObjectId(member._id);
+      const result = await this.productModel.find({
+        restaurant_mb_id: member._id,
+      });
+      assert.ok(result, Definer.general_err1);
+      return result;
+    } catch (err) {
+      throw err;
     }
+  }
 
-    async addNewProductData(data, member) {
-      try {
-        data.restaurant_mb_id = shapeIntoMongooseObjectId(member._id);
-        const new_product = new this.productModel(data);
-        const result = await new_product.save();
+  async addNewProductData(data, member) {
+    try {
+      data.restaurant_mb_id = shapeIntoMongooseObjectId(member._id);
 
-        assert.ok(result, Definer.product_err1);
-        return result;
+      const new_product = new this.productModel(data);
+      const result = await new_product.save();
 
-      } catch (err) {
-        throw err;
-      }
+      assert.ok(result, Definer.product_err1);
+      return result;
+    } catch (err) {
+      throw err;
     }
+  }
 
-    async updateChosenProductData(id, updated_data, mb_id) {
-        try {
-          id = shapeIntoMongooseObjectId(id);
-          mb_id = shapeIntoMongooseObjectId(mb_id);
+  async updateChosenProductData(id, updated_data, mb_id) {
+    try {
+      id = shapeIntoMongooseObjectId(id);
+      mb_id = shapeIntoMongooseObjectId(mb_id);
 
-          const result = await this.productModel
-          .findOneAndUpdate({_id: id, restaurant_mb_id: mb_id }, updated_data, {
-             runValidators: true,
-             lean: true,
-             returnDocument: "after",   
-            }
-          ).exec();
+      const result = await this.productModel
+        .findOneAndUpdate({ _id: id, restaurant_mb_id: mb_id }, updated_data, {
+          runValidators: true,
+          lean: true,
+          returnDocument: "after", // before
+        })
+        .exec();
 
-          assert.ok(result, Definer.general_err1);
-          return result;
-  
-        } catch (err) {
-          throw err;
-        }
-      }
-
-
+      assert.ok(result, Definer.general_err1);
+      return result;
+    } catch (err) {
+      throw err;
     }
+  }
+}
 
 module.exports = Product;
