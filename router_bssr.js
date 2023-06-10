fcf6510fc09626cae3f8e33f6e83c8b7afd3a4f2
Sardor Khadjimatov <sardorxadjimatov95@gmail.com>
2023-06-10 18:05:18 +0900

    feat: develop restaurant users editing their products

--- router_bssr.js
+++ router_bssr.js
@@ -24,6 +24,9 @@ router_bssr.post(
     upload_product.array("product_images", 5),
     productController.addNewProduct
     );
-router_bssr.post("/products/edit/:id", productController.updateChosenProduct);
+router_bssr.post("/products/edit/:id", 
+restaurantController.validateAuthRestaurant,
+productController.updateChosenProduct
+);
 
 module.exports = router_bssr;
